22b08c1fd9cf30593adf0f992fde5ff2ce0137bc
MathiasD-P <mathias.dufresne-piche@mail.mcgill.ca>
2026-04-06 20:10:39 -0400

    Type modification for ForwardDiff.jl

---
+++ src/jacobian_residual.jl
@@ -0,0 +1,11 @@
+#####################################################################
+# Evaluate the jacobian of the residual for our DG implementations
+#####################################################################
+
+function residual_jacobian()
+    # First, we wrap the build_residual! function
+    function residual_wrapper(u)
+        
+    end
+
+end
